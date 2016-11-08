4e50f9a8035a2d2ad45098223a7ea75b7a52d506
Katie Cunningham <katieirenec@gmail.com>
2016-11-07 22:27:49 -0500

    Update pavement.py with attributes needed for autoupdate

--- pavement.py
+++ pavement.py
@@ -2,6 +2,7 @@ import paver
 from paver.easy import *
 import paver.setuputils
 paver.setuputils.install_distutils_tasks()
+from socket import gethostname
 import os, sys
 
 from sphinxcontrib import paverutils
@@ -9,7 +10,14 @@ from sphinxcontrib import paverutils
 sys.path.append(os.getcwd())
 
 home_dir = os.getcwd()
-master_url = 'http://127.0.0.1:8000'
+
+master_url = None
+if master_url is None:
+    if gethostname() in  ['web407.webfaction.com', 'rsbuilder']:
+        master_url = 'http://interactivepython.org'
+    else:
+        master_url = 'http://127.0.0.1:8000'
+
 master_app = 'runestone'
 serving_dir = "./build/StudentCSP"
 
