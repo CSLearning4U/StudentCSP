2e51c7baee63e7cd13b5609e8765af3168dac2ea
Brad Miller <bonelake@mac.com>
2019-10-21 22:24:07 +0000

    give proper title

--- conf.py
+++ conf.py
@@ -163,7 +163,7 @@ html_theme_path = [pkg_resources.resource_filename('runestone', 'common/project_
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'AP CSP'
+html_title = 'AP CS Principles - Student Edition'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title ='AP CSP'
